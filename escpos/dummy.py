1194d5577095c7580ce9708822a10e19f84062e8
Luis Felipe Miléo <mileo@kmee.com.br>
2017-12-14 10:08:57 -0200

    Added Dummy and File connections
    Add FileConnection for printing on devices attached directly to the file system.
    
    Add DummyConnection for debugging and other development purposes.

---
+++ escpos/dummy.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+# escpos/fdummy.py
+#
+# Copyright 2017 KMEE INFORMATICA LTDA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+class DummyConnection(object):
+    """ Dummy printer
+
+    This class is used for saving commands to a variable, for use in situations where
+    there is no need to send commands to an actual printer. This includes
+    generating print jobs for later use, or testing output.
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        """
+        super(DummyConnection, self).__init__()
+        self._output_list = []
+
+
+
+    def write(self, data):
+        """ Print any command sent in raw format
+
+        :param msg: arbitrary code to be printed
+        :type msg: bytes
+        """
+        self._output_list.append(data)
+
+    @property
+    def output(self):
+        """ Get the data that was sent to this printer """
+        return b''.join(self._output_list)
+
+    def close(self):
+        pass
+
+    def catch(self):
+        pass
+
+    def read(self):
+        pass
---
+++ escpos/file.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+#
+# escpos/file.py
+#
+# Copyright 2017 KMEE INFORMATICA LTDA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+class FileConnection(object):
+    def __init__(self, devfile="/dev/usb/lp0",
+                 auto_flush=True,
+                 *args, **kwargs):
+
+        super(FileConnection, self).__init__()
+
+        self.devfile = devfile
+        self.auto_flush = auto_flush
+        self.open()
+
+    def open(self):
+        """ Open system file """
+        self.device = open(self.devfile, "wb")
+
+        if self.device is None:
+            print("Could not open the specified file {0}".format(self.devfile))
+
+    def flush(self):
+        """ Flush printing content """
+        self.device.flush()
+
+    def write(self, data):
+        """ Print any command sent in raw format
+
+        :param msg: arbitrary code to be printed
+        :type msg: bytes
+        """
+        self.device.write(data)
+        if self.auto_flush:
+            self.flush()
+
+    def close(self):
+        """ Close system file """
+        if self.device is not None:
+            self.device.flush()
+            self.device.close()
+
+    def catch(self):
+        return True
+
+    def read(self):
+	pass
